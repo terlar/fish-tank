0717acefc3ea24358beb9e0cd4da1773482f0899
Terje Larsen <terlar@gmail.com>
2014-12-14 18:19:49 +0100

    bugfix: shuf is not always available
    Some systems don't have `shuf`, so then we can fallback to `awk`.

--- share/fish-tank/tank.fish
+++ share/fish-tank/tank.fish
@@ -5,9 +5,18 @@ if not contains $tank_path/functions $fish_function_path
 	set fish_function_path $tank_path/functions $fish_function_path
 end
 
-if not type -t source >/dev/null
+if not type -q source
 	function source
 		. $argv
+	end
+end
+
+if not type -q shuf
+	function shuf
+		cat /dev/stdin \
+			| awk 'BEGIN{srand()}{printf "%06d %s\n", rand()*1000000, $0;}' \
+			| sort -n \
+			| cut -c8-
 	end
 end
 
