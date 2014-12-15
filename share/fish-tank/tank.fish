0d1256909476a70ac8fe1e7b13ff1cbd3bd0a978
Terje Larsen <terlar@gmail.com>
2014-12-15 07:38:02 +0100

    fix: deprecation methods can't rely on newer code
    In the version of fish where `source` didn't exist, there was also no
    `type -q`. That was introduced later. Therefore we have to fallback to
    the old ways.

--- share/fish-tank/tank.fish
+++ share/fish-tank/tank.fish
@@ -5,13 +5,13 @@ if not contains $tank_path/functions $fish_function_path
 	set fish_function_path $tank_path/functions $fish_function_path
 end
 
-if not type -q source
+if not type -t source >/dev/null ^/dev/null
 	function source
 		. $argv
 	end
 end
 
-if not type -q shuf
+if not type -f shuf >/dev/null ^/dev/null
 	function shuf
 		cat /dev/stdin \
 			| awk 'BEGIN{srand()}{printf "%06d %s\n", rand()*1000000, $0;}' \
